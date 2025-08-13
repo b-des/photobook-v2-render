b0410dd4e91705a7307d5ec669569b80a406fb9f
andrii <alltelex@gmail.com>
2025-08-13 15:42:15 +0300

    refactor

--- src/services/photobook.service.js
+++ src/services/photobook.service.js
@@ -48,7 +48,7 @@ const startRender = async (domain, uid, totalPages, width, height, withBorder) =
         const destFile = `${destinationPath}/${currentPage}.jpg`;
         await page.goto(url);
         await page.setViewport({
-            width: browserWidth,
+            width: browserWidth + page.getCoverExtraWidth(),
             height: browserHeight
         });
         await page.screenshot(destFile);
